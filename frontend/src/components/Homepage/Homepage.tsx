6d7e4652c0c7e407d59ad703979823e7b99a1f21
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-11-21 23:22:01 +0200

    Adding Pokemon list component and logic
    * Adding Pokemon list component and logic
    
    * remove comments

--- frontend/src/components/Homepage/Homepage.tsx
+++ frontend/src/components/Homepage/Homepage.tsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+
+import {useAuth} from "../../context/AuthContext";
 import "./Homepage.css";
 
 const Homepage: React.FC = () => {
     const navigate = useNavigate();
+    const {isLoggedIn} = useAuth();
 
     return (
         <div className="homepage">
             <header className="homepage-header">
                 <h1>Welcome to Pokemon App</h1>
-                <p>Your favorite pokemon app!.</p>
+                <p>Your favorite pokemon app!</p>
             </header>
             <div className="homepage-buttons">
-                <button className="btn btn-login" onClick={() => navigate("/login")}>
-                    Login
-                </button>
-                <button className="btn btn-signup" onClick={() => navigate("/signup")}>
-                    Signup
-                </button>
+                {!isLoggedIn ? (
+                    <>
+                        <button className="btn btn-login" onClick={() => navigate("/login")}>
+                            Login
+                        </button>
+                        <button className="btn btn-signup" onClick={() => navigate("/signup")}>
+                            Signup
+                        </button>
+                    </>
+                ) : (
+                    <button className="btn btn-pokemons" onClick={() => navigate("/pokemons")}>
+                        View Pokémons
+                    </button>
+                )}
             </div>
         </div>
     );
