1a0c2e72fe77e9cef77169544ae289b968ecf7f7
Idan Levian <eidan47@gmail.com>
2024-11-24 17:06:17 +0200

    Fix CORS and add search api to client

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 
 bootstrap();
