db1f4ef4ba3b34d8bb8dafddb143e597f40f7211
Idan Levian <eidan47@gmail.com>
2024-11-25 22:57:14 +0200

    Working docker

--- backend/src/cache/redis-cache.module.ts
+++ backend/src/cache/redis-cache.module.ts
@@ -8,7 +8,10 @@ import { ioRedisStore } from '@tirke/node-cache-manager-ioredis';
       isGlobal: true,
       useFactory: () => ({
         store: ioRedisStore,
-        host: process.env.REDIS_HOST || 'redis://127.0.0.1:6379/0',
+        host:
+          process.env.NODE_ENV === 'development'
+            ? 'localhost'
+            : process.env.REDIS_HOST || 'redis',
         port: parseInt(process.env.REDIS_PORT) || 6379,
         ttl: parseInt(process.env.REDIS_TTL) || 300,
       }),
