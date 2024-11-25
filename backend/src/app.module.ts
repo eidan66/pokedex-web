edacaaed94c5baadd6848ea3a1f04464a90cc3d4
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-11-25 15:01:47 +0200

    Adding pokemon schema and cache
    * Fix CORS and add search api to client
    
    * Adding redis caching
    
    * Update packages
    
    * Update code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { RedisCacheModule } from './cache/redis-cache.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
         synchronize: true,
       }),
     }),
+    RedisCacheModule,
     AuthModule,
     UserModule,
     PokemonModule,
