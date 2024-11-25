578b623ffcf63e76654e1759720e927d4408b18f
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-11-25 12:39:43 +0200

    Adding posgresql for users server and frontend
    * Fix CORS and add search api to client
    
    * Adding user login and registration apis
    
    * Adding Auth apis and implement on client
    
    * Remove comments
    
    * Adding postgresql to application and connect to client auth

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { User } from './user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
